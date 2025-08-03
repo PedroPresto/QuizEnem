695a149cd7736fb03bef1f6e3f982f06142e8a59
Pedro Presto <pedroprestodev@gmail.com>
2025-08-03 14:46:15 -0300

    integração com vps

--- src/java/controller/filter/AdminOnlyFilter.java
+++ src/java/controller/filter/AdminOnlyFilter.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import model.Usuario;
 
-@WebFilter(urlPatterns = {"/admins/*"})
+//@WebFilter(urlPatterns = {"/admins/*"})
 public class AdminOnlyFilter implements Filter {
 
     @Override
